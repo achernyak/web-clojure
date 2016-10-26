24f03eedbc0a353c3c5a49ff2dd6165ba0f2f4ad
achernyak <artem@ikayzo.com>
2016-10-26 06:47:53 -0500

    basic html templates

---
+++ html-templatin/src/html_templatin/core.clj
@@ -0,0 +1,30 @@
+(ns html-templatin.core
+  (:require [selmer.parser :as selmer]
+            [selmer.filters :as filters]))
+
+(selmer/render "Hello, {{name}}" {:name "World"})
+
+(selmer/render-file "hello.html" {:name "World"
+                                  :items (range 10)})
+
+(filters/add-filter! :empty? empty?)
+
+(selmer/render "{% if files|empty? %}no files{% else %}files{% endif %}"
+               {:files []})
+
+(filters/add-filter! :foo
+                     (fn [x] [:safe (.toUpperCase x)]))
+
+(selmer/render "{{x|foo}}" {:x "<div>I'm safe</div>"})
+
+(selmer/add-tag!
+ :image
+ (fn [args context-map]
+   (str "<img src=" (first args) "/>")))
+
+(selmer/render "{% image \"http://foo.com/logo.jpg\" %}" {})
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
